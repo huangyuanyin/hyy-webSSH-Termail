e7054ab900c364f08f07c62ba038c600a976f3a7
liuzhanghao <3443718488@qq.com>
2022-09-08 18:04:04 +0800

    fix

--- webssh/src/components/Console.vue
+++ webssh/src/components/Console.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="console" id="terminal"></div>
+  <div class="console" id="terminal"></div>
 </template>
 <script>
 import Terminal from './Xterm'
@@ -11,30 +11,30 @@ export default {
       default: {}
     }
   },
-  data () {
+  data() {
     return {
       term: null,
       terminalSocket: null
     }
   },
   methods: {
-    runRealTerminal () {
+    runRealTerminal() {
       console.log('webSocket is finished')
     },
-    errorRealTerminal () {
+    errorRealTerminal() {
       console.log('error')
     },
-    closeRealTerminal () {
+    closeRealTerminal() {
       console.log('close')
     }
   },
-  mounted () {
+  mounted() {
     console.log('pid : ' + this.terminal.pid + ' is on ready')
     let terminalContainer = document.getElementById('terminal')
     this.term = new Terminal()
     this.term.open(terminalContainer)
     // open websocket
-    this.terminalSocket = new WebSocket('ws://127.0.0.1:3000/terminals/')
+    this.terminalSocket = new WebSocket('ws://127.0.0.1:3003/terminals/') // 此地址端口号3003需与serve.py文件中的服务器端口号保持一致
     this.terminalSocket.onopen = this.runRealTerminal
     this.terminalSocket.onclose = this.closeRealTerminal
     this.terminalSocket.onerror = this.errorRealTerminal
@@ -42,7 +42,7 @@ export default {
     this.term._initialized = true
     console.log('mounted is going on')
   },
-  beforeDestroy () {
+  beforeDestroy() {
     this.terminalSocket.close()
     this.term.destroy()
   }
